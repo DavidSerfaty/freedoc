c398de51d07afde2c6c4f02861a9eaeedade8bf6
DavidSerfaty <david@owanted.io>
2020-10-22 15:54:30 +0200

    Add City and Specialty Models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 require 'faker'
 
 Doctor.destroy_all
 Patient.destroy_all
-
-specialities = ["Allergist", "Anesthesiologist", "Cardiologist", "Dermatologist", "Endocrinologist", "Gastroenterologist", "Hematologist", "Immunologist", "Internist"]
+City.destroy_all
+Specialty.destroy_all
 
 doctors = Array.new
 patients = Array.new
+cities = Array.new
+specialties = Array.new
+
+spes = ["Allergist", "Anesthesiologist", "Cardiologist", "Dermatologist", "Endocrinologist", "Gastroenterologist", "Hematologist", "Immunologist", "Internist"]
+
+spes.each do|specialty|
+  spe = Specialty.create(name: specialty)
+  specialties << spe
+end
+
+10.times do
+  city = City.create(name: Faker::Address.city)
+  cities << city
+end
 
 30.times do
-  doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: specialities.shuffle.first , zip_code: Faker::Address.zip_code)
-  patient =  Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Address.zip_code, city: cities.sample)
+  patient =  Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city: cities.sample)
   doctors << doctor
   patients << patient
 end
 
 30.times do
-  appointment = Appointment.create!(doctor: doctors.shuffle.first, patient: patients.shuffle.first, date: Faker::Date.between(from: '2020-11-01', to: '2020-12-01'))
+  appointment = Appointment.create!(doctor: doctors.sample, patient: patients.sample, date: Faker::Date.between(from: '2020-11-01', to: '2020-12-01'), city: cities.sample)
+  doctor_specialty = DoctorSpecialty.create(doctor: doctors.sample, specialty: specialties.sample)
 end
 
-puts "30 docteurs + 30 patients + 30 rendez-vous"
+puts "10 villes + 30 docteurs + 30 patients + 30 rendez-vous + 30 specialités"
